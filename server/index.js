b6822d7deb07a7f62478326e50a47f1dcb3403e4
nickrotondo <nickrotondo@gmail.com>
2020-09-23 23:23:00 -0700

    Set up database and server files. Build out server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+const dbHelpers = require('../database/dbHelpers.js');
+
+
+// Static files
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Middleware
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cors());
+
+// Get all reviews for particular campground
+app.get('/api/:campId', (req, res) => {
+  dbHelpers.getCampgroundReviews(req.params.campId, (err, results) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+// Set port and get confirmation
+let port = 3004;
+app.listen(port, () => console.log(`listening at port ${port}`));
