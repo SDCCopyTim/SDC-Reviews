874b6a5efa7da409eee07e35d3b8774a93bf9342
nickrotondo <nickrotondo@gmail.com>
2020-09-25 18:24:20 -0700

    Add function to query database for reviews by campId

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -1,2 +1,16 @@
 const db = require('./index.js');
 
+module.exports = {
+
+  // Get all reviews for particular campground by campgroundId.
+  getCampgroundReviews: (campId, callback) => {
+    db.Review.find({campgroundId: campId}, (err, results) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, results);
+      }
+    });
+  }
+
+};
